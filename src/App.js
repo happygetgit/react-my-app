204981a7aeea2f64f06baef3e61fcb97478a9974
happygetgit <sk39happy@gmail.com>
2025-03-22 11:24:52 +0530

    fixed issues related to hash router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import About from "./components/About";
-import React, { useState } from "react";
 import Alert from "./components/Alert";
-
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [modetext, setmodeText] = useState("Light Mode");
